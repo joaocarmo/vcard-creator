3ac804078b011f2f8834c7e11d45dad17695e5da
Joao Carmo <kingjoao@gmail.com>
2017-08-29 17:34:02 +0100

    Mostly working

---
+++ CHANGES.md
@@ -0,0 +1,4 @@
+## 0.0.0
+> Aug 29, 2017
+
+- Initial release
---
+++ test.js
@@ -0,0 +1,26 @@
+var { vCard } = require('./index')
+
+// define vcard
+var vcard = new vCard()
+
+// define variables
+var lastname = 'Desloovere'
+var firstname = 'Jeroen'
+var additional = ''
+var prefix = ''
+var suffix = ''
+
+// add personal data
+vcard.addName(lastname, firstname, additional, prefix, suffix)
+
+// add work data
+vcard.addCompany('Siesqo')
+vcard.addJobtitle('Web Developer')
+vcard.addRole('Data Protection Officer')
+vcard.addEmail('info@jeroendesloovere.be')
+vcard.addPhoneNumber(1234121212, 'PREF;WORK')
+vcard.addPhoneNumber(123456789, 'WORK')
+vcard.addAddress(null, null, 'street', 'worktown', null, 'workpostcode', 'Belgium')
+vcard.addURL('http://www.jeroendesloovere.be')
+
+console.log(vcard.toString())
