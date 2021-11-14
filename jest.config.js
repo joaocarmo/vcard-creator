25fa94426d8f605ce9cdbe4f6b723a4c94a9d451
João Carmo <kingjoao@gmail.com>
2021-11-14 22:02:55 +0000

    Added a testRegex to the Jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   modulePathIgnorePatterns: ['<rootDir>/test-functional/'],
   setupFiles: ['jest-date-mock'],
+  testRegex: '(/__tests__/.*)\\.test\\.[jt]s$',
 }
